5d4b40cf062f47b3717ab2bc4c1bfca9e1c7dec3
Frank Bouwens <frankkie12345@gmail.com>
2014-11-30 22:14:03 +0100

    Making a new Alpha release.

--- app/src/main/java/nl/frankkie/convention/data/EventDbHelper.java
+++ app/src/main/java/nl/frankkie/convention/data/EventDbHelper.java
@@ -11,7 +11,7 @@ import nl.frankkie.convention.data.EventContract.*;
  */
 public class EventDbHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "events.db";
 
     public EventDbHelper(Context context){
