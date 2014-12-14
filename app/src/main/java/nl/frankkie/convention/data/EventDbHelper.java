cf43752cdbbeccc17d881b61e77d707d933438b5
Frank Bouwens <frankkie12345@gmail.com>
2014-12-14 21:36:55 +0100

    Nieuw APK gemaakt. versionCode=6; DBVersion=5

--- app/src/main/java/nl/frankkie/convention/data/EventDbHelper.java
+++ app/src/main/java/nl/frankkie/convention/data/EventDbHelper.java
@@ -10,7 +10,7 @@ import nl.frankkie.convention.data.EventContract.*;
  */
 public class EventDbHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 4;
+    public static final int DATABASE_VERSION = 5;
     public static final String DATABASE_NAME = "events.db";
 
     public EventDbHelper(Context context){
